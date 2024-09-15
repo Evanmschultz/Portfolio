8e32a1f3f832fbf02a6a6fac3d75b900eae7345a
Evan <evanmschultz@gmail.com>
2024-09-15 16:07:56 -0600

    Adds about section

--- static/js/script.js
+++ static/js/script.js
@@ -2,8 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const canvas = document.getElementById('myCanvas');
     const projectsModal = document.getElementById('projects-modal');
     const contactModal = document.getElementById('contact-modal');
+    const aboutModal = document.getElementById('about-modal');
     const projectsBtn = document.getElementById('projects-button');
     const contactBtn = document.getElementById('contact-button');
+    const aboutBtn = document.getElementById('about-button');
     const closeButtons = document.getElementsByClassName('close-button');
     
     // Function to reveal canvas
@@ -14,42 +16,43 @@ document.addEventListener('DOMContentLoaded', function() {
     // Reveal canvas immediately after page load
     revealCanvas();
 
-    // Or, Reveal canvas after a short delay, in milliseconds, if the above doesn't work
-    // setTimeout(revealCanvas, 300);
-	
-	function openModal(modal) {
-		modal.style.display = 'block';
-		setTimeout(() => {
-			modal.style.opacity = '1';
-			modal.querySelector('.modal-content').style.transform = 'translateY(0)';
-		}, 10);
-	}
-	
-	function closeModal(modal) {
-		modal.querySelector('.modal-content').style.transform = 'translateY(-50px)';
-		modal.style.opacity = '0';
-		setTimeout(() => {
-			modal.style.display = 'none';
-		}, 300);
-	}
-	
-	projectsBtn.onclick = function() {
-		openModal(projectsModal);
-	}
-	
-	contactBtn.onclick = function() {
-		openModal(contactModal);
-	}
-	
-	Array.from(closeButtons).forEach(button => {
-		button.onclick = function() {
-			closeModal(this.closest('.modal'));
-		}
-	});
-	
-	window.onclick = function(event) {
-		if (event.target.classList.contains('modal')) {
-			closeModal(event.target);
-		}
-	}
+    function openModal(modal) {
+        modal.style.display = 'grid'; 
+        setTimeout(() => {
+            modal.style.opacity = '1';
+            modal.querySelector('.modal-content').style.transform = 'translateY(0)';
+        }, 10);
+    }
+    
+    function closeModal(modal) {
+        modal.querySelector('.modal-content').style.transform = 'translateY(-50px)';
+        modal.style.opacity = '0';
+        setTimeout(() => {
+            modal.style.display = 'none';
+        }, 300);
+    }
+    
+    projectsBtn.onclick = function() {
+        openModal(projectsModal);
+    }
+    
+    contactBtn.onclick = function() {
+        openModal(contactModal);
+    }
+
+    aboutBtn.onclick = function() {
+        openModal(aboutModal);
+    }
+    
+    Array.from(closeButtons).forEach(button => {
+        button.onclick = function() {
+            closeModal(this.closest('.modal'));
+        }
+    });
+    
+    window.onclick = function(event) {
+        if (event.target.classList.contains('modal')) {
+            closeModal(event.target);
+        }
+    }
 });
